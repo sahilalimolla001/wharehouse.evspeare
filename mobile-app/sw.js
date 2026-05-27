e83f49ecaee0019a70f95fc19db1af742233a5de
Sahil Ali <sahilalimolla001@gmail.com>
2026-05-27 15:39:54 +0530

    Refresh picker offline app cache

--- mobile-app/sw.js
+++ mobile-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "evsphere-picker-v14";
+const CACHE_NAME = "evsphere-picker-v15";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
